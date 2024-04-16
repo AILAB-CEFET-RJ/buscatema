2d87895c5f2075fb1dd42a7ee0a92d3267e9fe22
renancarvalhops <renancps77@gmail.com>
2024-04-15 21:22:41 -0300

    refactor: UploadZone

--- ui/src/components/UploadZone.jsx
+++ ui/src/components/UploadZone.jsx
@@ -10,11 +10,6 @@ export function UploadZone() {
         csv: ''
     })
 
-    const [extractedText, setExtractedText] = useState({
-        pdf: '',
-        csv: ''
-    })
-
     const setInfo = useContext(InfoContext)
 
     const filesUploaded = Object.values(files).reduce((prev, curr) => {
@@ -26,43 +21,22 @@ export function UploadZone() {
         formData.append('pdf', files.pdf)
         formData.append('csv', files.csv)
 
-        try {
-            const response = await fetch('http://localhost:5000/upload', {
-                method: 'POST',
-                body: formData
-            })
-    
-            if (response.ok) {
-                const data = await response.json()
-                console.log(data.message) // Exibe a mensagem do backend no console do navegador
-                console.log(data.pdf_text) // Exibe o texto extraído do PDF no console do navegador
-                console.log(data.csv_text) // Exibe o texto extraído do CSV no console do navegador
-                setInfo(data) // Atualiza o estado com a resposta do backend, se necessário
-                setExtractedText({
-                    pdf: data.pdf_text,
-                    csv: data.csv_text
-                }) // Atualiza o estado com o texto extraído do PDF e do CSV
-                setFiles({ pdf: '', csv: '' }) // Limpa os arquivos após o envio bem-sucedido
-            } else {
-                console.error('Erro ao enviar os arquivos:', response.statusText)
-                // Trate o erro conforme necessário
-            }
-        } catch (error) {
-            console.error('Erro ao enviar os arquivos:', error)
-            // Trate o erro conforme necessário
-        }
+        const res = await fetch(`http://localhost:5000/upload`, {
+            method: 'POST',
+            body: formData
+        })
+        
+        setInfo(res.ok ? await res.json() : [])
+        setFiles({ pdf: '', csv: '' })
     }
 
     const button = filesUploaded >=2 ? 
     <Button variant="contained" onClick={sendFiles}>
-        Enviar
+        enviar
     </Button> :
     <Button variant="contained" disabled>
         Arquivos inseridos ({filesUploaded} de 2)
     </Button>
-
-    console.log('Extracted Text PDF:', extractedText.pdf); // Adiciona esta linha para depuração
-    console.log('Extracted Text CSV:', extractedText.csv); // Adiciona esta linha para depuração
 
     return (
     <div className='uploadzone'>
@@ -72,12 +46,6 @@ export function UploadZone() {
         </div>
 
         {button}
-
-        {/* Exibir o texto extraído do PDF */}
-        {extractedText.pdf && <div>{extractedText.pdf}</div>}
-        
-        {/* Exibir o texto extraído do CSV */}
-        {extractedText.csv && <div>{extractedText.csv}</div>}
     </div>
     )
 }
