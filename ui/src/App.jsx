f1b7198f6fcba3cb29b1ac2bb5be175fd2aab1e9
Leticia <leticiamtavora@gmail.com>
2024-04-17 17:58:04 -0300

    Revert "Show themes/Front-Back integration improved"
    This reverts commit 72929bc1beec84af861adee95ab219583f7fe7ce.

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,53 +2,22 @@ import Container from '@mui/material/Container'
 import { UploadZone } from './components/UploadZone'
 import { ThemeCard } from './components/ThemeCard'
 import './styles/dropzone.css'
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState } from 'react'
 
 export const InfoContext = createContext()
 
 export default function App() {
-    const [info, setInfo] = useState([]);
+    const [info, setInfo] = useState([])
 
-    const fetchInfoFromBackend = async (formData) => {
-        try {
-            const response = await fetch('/upload', {
-                method: 'POST',
-                body: formData
-            });
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            setOrderedInfo(data);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
-
-    const handleFileUpload = (pdfFile, csvFile) => {
-        const formData = new FormData();
-        formData.append('pdf', pdfFile);
-        formData.append('csv', csvFile);
-        fetchInfoFromBackend(formData);
-    };
-
-    useEffect(() => {
-        fetchInfoFromBackend();
-    }, []); 
-
-    const setOrderedInfo = (info) => {
-        if (Array.isArray(info)) {
-            const orderedInfo = info.sort((a, b) => b.likeliness - a.likeliness)
-            setInfo(orderedInfo);
-        } else {
-            console.error('Data received from backend is not an array');
-        }
+    const setOrderedInfo = (info) => { 
+        const orderedInfo = info.sort((a, b) => b.likeliness - a.likeliness)
+        setInfo(orderedInfo);
     };
 
     return (
     <InfoContext.Provider value={setOrderedInfo}>
         <Container maxWidth="lg" sx={{ p: 5 }}>
-            <UploadZone onFileUpload={handleFileUpload} />
+            <UploadZone/>
 
             <hr style={{border: '1px solid #ddd', margin: '2rem 0 4rem 0'}}/>
 
