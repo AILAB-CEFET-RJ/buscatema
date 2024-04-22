571af84d653b1cc76ec905087c70b325eb02452f
renancarvalhops <renancps77@gmail.com>
2024-04-22 12:46:38 -0300

    fix: informação transmitida ao frontend

--- backend/app.py
+++ backend/app.py
@@ -37,9 +37,18 @@ def upload_file():
         pdf_file.save(pdf_path)
         csv_file.save(csv_path)
         # Retornar uma resposta ao frontend
-        data = [{'message': 'Arquivos recebidos com sucesso',
-                        'pdf_filename': pdf_filename,
-                        'csv_filename': csv_filename}]
+        data = [
+            {
+            "content": "Definir o âmbito de aplicação do artigo 112 da Lei 8.213/91; Definir se o artigo 112 da Lei 8.213/91 versa sobre hipótese de sucessão processual, na forma do artigo 110 do CPC (\"Art. 110. Ocorrendo a morte de qualquer das partes, dar-se-á a sucessão pelo seu espólio ou pelos seus sucessores, observado o disposto no art. 313, §§ 1º e 2º\"); Não sendo hipótese de sucessão processual: a) definir se os pensionistas possuem direito de pleitear, em nome próprio, valores não recebidos em vida pelo falecido segurado; b) definir se, na ausência dos pensionistas, os herdeiros e/ou espólio possuem direito de pleitear, em nome próprio, parcelas não recebidas em vida pelo falecido segurado; Definir em que consiste \"o valor não recebido em vida pelo segurado\" mencionado no artigo 112 da Lei 8.213/91, isto é, se se tratam de importâncias não recebidas em vida pelo de cujus, mas já integradas ao seu patrimônio (por exemplo: indeferimento de requerimento administrativo de concessão/revisão de benefício e cancelamento indevido de benefício) ou simplesmente qualquer pagamento a menor relativo ao benefício previdenciário do falecido e que não foi reclamado administrativa ou judicialmente por ele em vida.",
+            "id": 1,
+            "likeliness": 65
+            },
+            {
+            "content": "Definir o âmbito de aplicação do artigo 112 da Lei 8.213/91;  Definir se o artigo 112 da Lei 8.213/91 versa sobre hipótese de sucessão processual, na forma do artigo 110 do CPC (\"Art. 110. Ocorrendo a morte de qualquer das partes, dar-se-á a sucessão pelo seu espólio ou pelos seus sucessores, observado o disposto no art. 313, §§ 1º e 2º\");  Não sendo hipótese de sucessão processual: a) definir se os pensionistas possuem direito de pleitear, em nome próprio, valores não recebidos em vida pelo falecido segurado; b) definir se, na ausência dos pensionistas, os herdeiros e/ou espólio possuem direito de pleitear, em nome próprio, parcelas não recebidas em vida pelo falecido segurado;  Definir em que consiste \"o valor não recebido em vida pelo segurado\" mencionado no artigo 112 da Lei 8.213/91, isto é, se se tratam de importâncias não recebidas em vida pelo de cujus, mas já integradas ao seu patrimônio (por exemplo: indeferimento de requerimento administrativo de concessão/revisão de benefício e cancelamento indevido de benefício) ou simplesmente qualquer pagamento a menor relativo ao benefício previdenciário do falecido e que não foi reclamado administrativa ou judicialmente por ele em vida.",
+            "id": 2,
+            "likeliness": 90
+            }
+        ]
         return jsonify(data), 200
     else:
         return jsonify({'error': 'Tipos de arquivo não permitidos'}), 400
